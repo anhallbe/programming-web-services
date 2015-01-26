9ff348c466f573cae3b78ac3f79c3c73229efcb9
Andreas Hallberg <anhallbe@gmail.com>
2015-01-26 06:05:17 +0100

    init schema files

---
+++ pws-hw1/src/pws/hw1/Main.java
@@ -0,0 +1,17 @@
+package pws.hw1;
+
+/**
+ *
+ * @author andreas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
