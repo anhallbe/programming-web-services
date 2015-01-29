a9d73ab56e774ad16c95781c1d4df1fa25688c5a
Andreas Hallberg <anhallbe@gmail.com>
2015-01-29 13:49:19 +0100

    sax parser done

--- pws-hw1/src/pws/hw1/sax/SAXPParser.java
+++ pws-hw1/src/pws/hw1/sax/SAXPParser.java
@@ -17,12 +17,13 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public class SAXPParser {
     
-    public SAXPParser() {
+    public static Transcript parseTranscript() {
+        SAXTranscriptHandler handler = new SAXTranscriptHandler();
         try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
             
-            saxParser.parse(new File("/home/andreas/Development/programming-web-services/pws-hw1/src/pws/hw1/xml/hogwarts-transcript.xmll"), new SAXTranscriptHandler());
+            saxParser.parse(new File("/home/andreas/Development/programming-web-services/pws-hw1/src/pws/hw1/xml/hogwarts-transcript.xml"), handler);
             
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(SAXPParser.class.getName()).log(Level.SEVERE, null, ex);
@@ -32,35 +33,65 @@ public class SAXPParser {
             Logger.getLogger(SAXPParser.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        
+        return handler.transcript;
     }
     
-    private class SAXTranscriptHandler extends DefaultHandler {
+//    public static void main(String[] args) {
+//        Transcript transcript = SAXPParser.parseTranscript();
+//        System.out.println(transcript);
+//    }
+    
+    private static class SAXTranscriptHandler extends DefaultHandler {
         public Transcript transcript = new Transcript();
-        
-        @Override
-        public void startDocument() throws SAXException {
-            super.startDocument(); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void endDocument() throws SAXException {
-            super.endDocument(); //To change body of generated methods, choose Tools | Templates.
-        }
+        private Course course;
+        private String content = "";
 
         @Override
         public void characters(char[] ch, int start, int length) throws SAXException {
-            super.characters(ch, start, length); //To change body of generated methods, choose Tools | Templates.
+            content = String.copyValueOf(ch, start, length).trim();
         }
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            super.startElement(uri, localName, qName, attributes); //To change body of generated methods, choose Tools | Templates.
+            switch(qName) {
+                case "courses":
+                    course = new Course();
+                    break;
+                case "transcript":
+                    transcript = new Transcript();
+                    break;
+            }
+            content = "";
         }
 
         @Override
         public void endElement(String uri, String localName, String qName) throws SAXException {
-            super.endElement(uri, localName, qName); //To change body of generated methods, choose Tools | Templates.
+            switch(qName) {
+                case "name":
+                    transcript.setName(content);
+                    break;
+                case "university":
+                    transcript.setUniversity(content);
+                    break;
+                case "degree":
+                    transcript.setDegree(content);
+                    break;
+                case "year":
+                    transcript.setYear(Integer.parseInt(content));
+                    break;
+                case "courseName":
+                    course.setName(content);
+                    break;
+                case "courseCode":
+                    course.setCode(content);
+                    break;
+                case "grade":
+                    course.setGrade(Integer.parseInt(content));
+                    break;
+                case "courses":
+                    transcript.addCourse(course);
+                    break;
+            }
         }
     }
 }
