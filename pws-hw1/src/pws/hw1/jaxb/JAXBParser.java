491f6d47c6b518e1a2e3e84c861ecde50995eae1
Andreas Hallberg <anhallbe@gmail.com>
2015-01-30 09:29:30 +0100

    JAXB Parser

---
+++ pws-hw1/src/pws/hw1/jaxb/JAXBParser.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pws.hw1.jaxb;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ *
+ * @author andreas
+ */
+public class JAXBParser {
+    public static CompanyType parseCompany() {
+        try {
+            JAXBContext jc = JAXBContext.newInstance("pws.hw1.jaxb");
+            Unmarshaller u = jc.createUnmarshaller();
+            Source source = new StreamSource(new File("/home/andreas/Development/programming-web-services/pws-hw1/src/pws/hw1/xml/cauldrons-info.xml"));
+            JAXBElement<CompanyType> root = u.unmarshal(source, CompanyType.class);
+            CompanyType company = root.getValue();
+            
+            System.out.println(company.getName());
+            System.out.println(company.getDescription());
+            System.out.println(company.getKeywords());
+        } catch (JAXBException ex) {
+            Logger.getLogger(JAXBParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        parseCompany();
+    }
+}
