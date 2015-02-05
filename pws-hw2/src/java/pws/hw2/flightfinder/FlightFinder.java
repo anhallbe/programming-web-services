609de82c3787c2fb2597bd0ca8536e79b0e4c838
Andreas Hallberg <anhallbe@gmail.com>
2015-02-05 20:54:45 +0100

    Flightfinder refactor

--- pws-hw2/src/java/pws/hw2/flightfinder/FlightFinder.java
+++ pws-hw2/src/java/pws/hw2/flightfinder/FlightFinder.java
@@ -27,15 +27,15 @@ public class FlightFinder {
     
     public FlightFinder() {
         allFlights = new ArrayList<>();
-        allFlights.add(new Flight("A", "B"));
-        allFlights.add(new Flight("B", "C"));
-        allFlights.add(new Flight("A", "C"));
-        allFlights.add(new Flight("C", "A"));
-        allFlights.add(new Flight("A", "D"));
-        allFlights.add(new Flight("B", "D"));
-        allFlights.add(new Flight("C", "D"));
-        allFlights.add(new Flight("D", "C"));
-        allFlights.add(new Flight("D", "A"));
+        allFlights.add(new Flight("Stockholm", "Madrid"));
+        allFlights.add(new Flight("Madrid", "London"));
+        allFlights.add(new Flight("Stockholm", "London"));
+        allFlights.add(new Flight("London", "Stockholm"));
+        allFlights.add(new Flight("Stockholm", "Paris"));
+        allFlights.add(new Flight("Madrid", "Paris"));
+        allFlights.add(new Flight("London", "Paris"));
+        allFlights.add(new Flight("Paris", "London"));
+        allFlights.add(new Flight("Paris", "Stockholm"));
 //        allFlights.add(new Flight("Oslo", "Stockholm"));
 //        allFlights.add(new Flight("Stockholm", "Oslo"));
 //        allFlights.add(new Flight("Oslo", "Stockholm"));
@@ -79,16 +79,15 @@ public class FlightFinder {
      */
     @WebMethod(operationName = "possibleItineraries")
     public List<Itinerary> possibleItineraries(@WebParam(name = "departure") String departure, @WebParam(name = "destination") String destination) {
-//        findItineraries(s);
-//        return itineraries;
         List<Flight> fl = new ArrayList<>();
-        fl.addAll(findPaths(departure, destination, new ArrayList<>(), new ArrayList<>()));
+        fl.addAll(findPaths(departure, destination));
+//        fl.addAll(findPaths(departure, destination, new ArrayList<>(), new ArrayList<>()));
         List<Itinerary> result = structurePaths(fl, departure, destination);
         return result;
-//        Itinerary i = new Itinerary(fl);
-//        List<Itinerary> l = new ArrayList<>();
-//        l.add(i);
-//        return l;
+    }
+    
+    private List<Flight> findPaths(String from, String to) {
+        return findPaths(from, to, new ArrayList<>(), new ArrayList<>());
     }
     
     private List<Flight> findPaths(String current, String dest, List<Flight> flightsTaken, List<String> visitedCities) {
@@ -97,9 +96,7 @@ public class FlightFinder {
         
         List<Flight> path = new ArrayList<>();
         for(Flight f : allFlights) {
-//            if(f.getFrom().equals(current) && !flightsTaken.contains(f)) {
             if(f.getFrom().equals(current) && !visitedCities.contains(f.getTo())) {
-                //result = result + ...
                 List<Flight> newVisited = new ArrayList<>(flightsTaken);
                 newVisited.add(f);
                 List<String> newVisitedCities = new ArrayList<>(visitedCities);
@@ -127,34 +124,4 @@ public class FlightFinder {
         }
         return itineraries;
     }
-    
-//    private List<Flight> findPaths(String current, String dest, List<Flight> visited) {
-//        if(current.equals(dest))
-//            return visited;
-//        
-//        List<Flight> path = new ArrayList<>();
-//        for(Flight f : allFlights) {
-//            if(f.getFrom().equals(current) && !visited.contains(f)) {
-//                //result = result + ...
-//                List<Flight> newVisited = new ArrayList<>(visited);
-//                newVisited.add(f);
-//                List<Flight> tail = findPaths(f.getTo(), dest, newVisited);
-//                path.addAll(tail);
-//            }
-//        }
-//        return path;
-//    }
-    
-    
-//    
-//    private List<Itinerary> findItiniraries(String from, String to, List<Flight> flights, List<Flight> flightsTaken, List<Itinerary> itineraries) {
-//        Flight flight = flights.remove(0);
-//        flightsTaken.add(flight);
-//        
-//        //If the current flight plan has reached the destination
-//        if(flightsTaken.get(0).getFrom().equals(from) && flightsTaken.get(flightsTaken.size()-1).getTo().equals(to)) {
-//            itineraries.add(new Itinerary(flightsTaken));
-//            return itineraries;
-//        }
-//    }
 }
