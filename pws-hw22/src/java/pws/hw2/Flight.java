bb74e1ace1276e898e86b9de028697da25f08188
Andreas Hallberg <anhallbe@gmail.com>
2015-02-09 11:55:38 +0100

    lots of refactoring, new project

---
+++ pws-hw22/src/java/pws/hw2/Flight.java
@@ -0,0 +1,73 @@
+package pws.hw2;
+
+
+public class Flight {
+    private String from;
+    private String to;
+    private int seats;
+    private int price;
+    private int flightID;
+
+    public static final int MAX_SEATS = 100;
+    public static final int MAX_PRICE = 1000;
+    
+    public Flight(String from, String to) {
+        this.from = from;
+        this.to = to;
+        seats = MAX_SEATS;
+        price = (int) (MAX_PRICE * Math.random());
+        flightID = (int) (Integer.MAX_VALUE * Math.random());
+    }
+    
+    /**
+     * Try to book this flight. Returns true if there were available seats, otherwise false.
+     * @return 
+     */
+    public boolean book() {
+        if(seats > 0) {
+            seats--;
+            return true;
+        } else
+            return false;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+    
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    
+    public int getID() {
+        return flightID;
+    }
+    
+    public void setID(int flightID) {
+        this.flightID = flightID;
+    }
+}
---
+++ pws-hw22/src/java/pws/hw2/TicketBooking.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pws.hw2;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.jws.WebService;
+
+/**
+ *
+ * @author andreas
+ */
+@WebService(serviceName = "TicketBooking", portName = "TicketBookingPort", endpointInterface = "pws.hw2.TicketBooking", targetNamespace = "http://hw2.pws/", wsdlLocation = "WEB-INF/wsdl/TicketBooking/TicketBooking.wsdl")
+public class TicketBooking {
+
+    public java.util.List<pws.hw2.Ticket> ticketBooking(java.lang.String creditCard, pws.hw2.Itinerary itinerary) {
+        List<pws.hw2.Ticket> tickets = new ArrayList<>();
+        
+        for(pws.hw2.Flight flight : itinerary.getFlights()) {
+            pws.hw2.Ticket ticket = new pws.hw2.Ticket();
+            ticket.setFlight(flight);
+            ticket.setBooked(flight.book());
+            tickets.add(ticket);
+        }
+        
+        return tickets;
+    }
+    
+}
